81ac5dc3ac9ea7bbf37610b30ed29ad491fd1b10
Travis Nesbit <travis@geekmd.io>
2018-08-13 21:32:43 -0700

    Refactored config to have less magic strings and for readability

--- print.go
+++ print.go
@@ -2,10 +2,13 @@ package main
 
 import "fmt"
 
+// Method for simplifying printing to console.
 func printHeading(formattedText string) {
 	fmt.Println(formattedText)
 }
 
+
+// Method for simplifying printing to console.
 func printSubtext(formattedText string) {
 	fmt.Println("- " + formattedText)
 }
